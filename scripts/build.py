eff2610e1ad045d5dc563b7e0abf8342ff878ce6
Ben <skoghoern@gmail.com>
2025-01-21 14:54:33 +0100

    removed alt from world image

--- scripts/build.py
+++ scripts/build.py
@@ -93,8 +93,7 @@ def generate_index(all_notebooks: List[str], output_dir: str) -> None:
       <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between items-center py-4">
           <div class="flex items-center space-x-8">
-            <img src="notebooks/public/active-inference-world-png.png" 
-                 alt="brain symbol" 
+            <img src="notebooks/public/active-inference-world.png" 
                  class="h-8 dark:invert" />
           </div>
           <div class="flex items-center space-x-4">
